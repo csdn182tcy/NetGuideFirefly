4a4bfcd6a7a7133aaa32f9e3843551e895c4d46e
陈杨 <18180464732@163.com>
2023-08-18 11:32:07 +0800

    删除测试文件

--- DotNetFps.Rc1/DotNetFps.Rc1/Data/Base/GlobalVariables.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DotNetFps.Rc1.Data.Base;
-
-public static class GlobalVariables
-{
-    public const string DefaultRoute = "dashboard/ecommerce";    
-}
--- DotNetFps.Rc1/DotNetFps.Rc1/Data/Shared/Favorite/FavoriteService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DotNetFps.Rc1.Data.Shared.Favorite
-{
-    public static class FavoriteService
-    {
-        public static List<int> GetDefaultFavoriteMenuList() => new() { 5, 2, 15 };
-    }
-}
